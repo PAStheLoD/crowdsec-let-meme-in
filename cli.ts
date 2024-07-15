b12cfb9d0f091fee20185c998f23ec55dc52c6b9
Pas <pas@zomg.hu>
2024-07-15 03:49:57 +0200

    CLI: remove dead code from Enum helper

--- cli.ts
+++ cli.ts
@@ -1,82 +1,75 @@
 type Enum<E> = Record<keyof E, number | string> & { [k: number]: string };
 
-const enumValues = <T,>(enu: Enum<T>) => {
-  const r = []
+const enumValues = <T>(enu: Enum<T>) => {
+  const r = [];
 
   for (const e in enu) {
     if (parseInt(e).toString() === e) {
-      r.push(enu[e])
+      r.push(enu[e]);
     }
   }
-  return r
-}
+  return r;
+};
 
+export const enumToCli = <E>(e: Enum<E>) => {
+  const features = enumValues(e);
 
-export const enumToCli = <E,>(e: Enum<E>) => { 
+  const good = new RegExp(/^[A-Za-z0-9_ -]+$/);
 
-  const features = enumValues(e)
+  type MenuItem = { name: string; regularizedName: string };
 
-  const good = new RegExp(/^[A-Za-z0-9_ -]+$/)
+  const menu: MenuItem[] = [];
 
-  type MenuItem = { name: string, regularizedName: string }
-
-  const menu: MenuItem[] = []
-
-  features.forEach(name => {
+  features.forEach((name) => {
     if (!good.test(name)) {
-      throw new Error(`internal error: feature names must not contain weird fucking characters. yet here we have: "${name}"   (good names match this RegExp ${good.source})`)
+      throw new Error(
+        `internal error: feature names must not contain weird fucking characters. yet here we have: "${name}"   (good names match this RegExp ${good.source})`,
+      );
     }
 
-
-    const menuItem = []
-    let word = []
-
-    const hasDash = (str: string) => str.startsWith('-') || str.endsWith('-')
+    const menuItem = [];
+    let word = [];
 
     for (let i = 0, len = name.length; i < len; i++) {
-      const ch = name[i]
+      const ch = name[i];
 
       const code = name.charCodeAt(i);
-      const prev = word.length > 0 ? word[word.length - 1] : ""
 
-
-      if (ch === '_' || ch === ' ') {
-
-        menuItem.push(word.join(""))
-        word = []
-
+      if (ch === "_" || ch === " ") {
+        menuItem.push(word.join(""));
+        word = [];
       } else if (code > 64 && code < 91) { // A-Z (uppercase), so we start a new word
+        menuItem.push(word.join(""));
+        word = [];
 
-        menuItem.push(word.join(""))
-        word = []
-
-        word.push(ch.toLowerCase())
-
+        word.push(ch.toLowerCase());
       } else {
-        word.push(ch)
+        word.push(ch);
       }
     }
 
     if (word.length > 0) {
-      menuItem.push(word.join(""))
+      menuItem.push(word.join(""));
     }
 
     const regularizedName = menuItem
-      .filter(x => x.length > 0)
-      .filter(x => x !== '-')
-      .map(part => (part.length == 2 && part[0] === '-') ? part[1] : part)
-      .map(part => (part.length == 2 && part[1] === '-') ? part[0] : part)
-      .join("-")
+      .filter((x) => x.length > 0)
+      .filter((x) => x !== "-")
+      .map((part) => (part.length == 2 && part[0] === "-") ? part[1] : part)
+      .map((part) => (part.length == 2 && part[1] === "-") ? part[0] : part)
+      .join("-");
 
-    if (menu.map(x => x.regularizedName).includes(regularizedName)) {
-      const colliding = menu.filter(x => x.regularizedName === regularizedName)[0]
-      throw new Error(`internal error: regularized feature names must be unique, yet here we are: ${name} regularizes to ${regularizedName} which is already in the menu (collides with ${colliding.name})`)
+    if (menu.map((x) => x.regularizedName).includes(regularizedName)) {
+      const colliding = menu.filter((x) =>
+        x.regularizedName === regularizedName
+      )[0];
+      throw new Error(
+        `internal error: regularized feature names must be unique, yet here we are: ${name} regularizes to ${regularizedName} which is already in the menu (collides with ${colliding.name})`,
+      );
     }
 
-    menu.push({ name, regularizedName })
-  })
+    menu.push({ name, regularizedName });
+  });
 
-  return menu
-
-}
-
+  return menu;
+};
